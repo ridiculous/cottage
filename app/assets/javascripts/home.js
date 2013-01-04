31b7c129f3981087aff835fa0bd42c1cc61123ca
Ryan Buckley <arebuckley@gmail.com>
2013-01-04 09:43:35 -1000

    Add rate calculator and g map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,19 @@
 $(function () {
+    $('#people_count').bind('keyup', function () {
+        var count = parseInt($(this).val().replace(/[^\d+]/, '')),
+            base_price = 175,
+            tax_rate = 0.135,
+            total = 175;
+
+        if (!count) count = 2;
+        switch (count) {
+            case 1:
+            case 2:
+                break;
+            default:
+                base_price = base_price + ((count - 2) * 25);
+        }
+        total = base_price + (base_price * tax_rate);
+        $('#rate_result').text(accounting.formatMoney(total));
+    });
 });
