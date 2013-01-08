3136749642c4115fb5b37d9bc9f09677c682180d
Ryan Buckley <arebuckley@gmail.com>
2013-01-07 15:57:43 -1000

    Minor style stuff and change email

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,11 @@
 module HomeHelper
   def section_header(title)
-    content_tag(:div, class: 'section') do
-      content_tag(:div, class: 'wrapper') do
-        concat link_to('', '#', {name: title.underscore.downcase})
-        concat content_tag(:h3, title)
+    content_tag(:div, class: 'section-wrap') do
+      content_tag(:div, class: 'section') do
+        content_tag(:div, class: 'wrapper') do
+          concat link_to('', '#', {name: title.underscore.downcase})
+          concat content_tag(:h3, title)
+        end
       end
     end
   end
