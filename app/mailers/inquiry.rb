a9c506d9bd4d35af1e5b9e325cf41de2e8b52a5f
Ryan Buckley <arebuckley@gmail.com>
2013-01-16 22:44:25 -1000

    Change recipients of inquiry email

--- app/mailers/inquiry.rb
+++ app/mailers/inquiry.rb
@@ -1,9 +1,9 @@
 class Inquiry < ActionMailer::Base
-  default :from => 'System - Tradewinds Cottage <info@tradewindscottage.net>'
-  # Brianna <brianna@tradewindscottage.net
+  default :from => 'Tradewinds Cottage <info@tradewindscottage.net>'
+
   def new_inquiry(contact)
     @contact = contact
-    mail(to: "Ryan <arebuckley@gmail.com>", subject: "New inquiry for the Tradewinds Cottage!")
+    mail(to: "Brianna <brianna@tradewindscottage.net, Ryan <arebuckley@gmail.com>", subject: "New inquiry for the Tradewinds Cottage!")
   end
 
 end
