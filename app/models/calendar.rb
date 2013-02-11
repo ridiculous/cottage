a6a22d6905e8d4a4cab33bab42d560064aa36a3a
Ryan Buckley <arebuckley@gmail.com>
2013-02-11 00:27:43 -1000

    Refactor +refresh_available_dates+

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -23,22 +23,26 @@ class Calendar < ActiveRecord::Base
     def refresh_available_dates
       agent = Mechanize.new do |a|
         a.user_agent_alias = 'Mac Safari'
-        a.redirection_limit = 100
+        a.redirection_limit = 10
         a.follow_meta_refresh = true
       end
       calendar = agent.get('https://www.homeawayconnect.com/calendar.cfm?pid=51970')
+      transaction do
+        # clear the table
+        delete_all
 
-      # map out available dates and save to DB as array
-      today = Date.today
-      last_day = today + 1.year
-      available_dates = []
-      today.upto(last_day).each do |date|
-        month = date.month.to_s.length == 1 ? "0#{date.month}" : date.month.to_s
-        table_cells = calendar.search("#calMonthAvail#{date.year}#{month}").search('td.ACADV, td.ACWDV')
-        available_dates << date.to_s if table_cells && table_cells.map { |cell| cell.children.to_s }.include?(date.day.to_s)
+        # map out available dates and save to DB as array
+        today = Date.today
+        tds = Hash.new
+        create(available_dates: today.upto(today + 1.year).map { |date|
+          m = date.month.to_s
+          table_cells = calendar.search("#calMonthAvail#{date.year}#{m.length == 1 ? "0#{m}" : m}").search('td.ACADV, td.ACWDV')
+          if table_cells
+            tds[m] ||= table_cells.map { |cell| cell.children.to_s }
+            date.to_s if tds[m].include?(date.day.to_s)
+          end
+        }.compact)
       end
-      delete_all
-      create(available_dates: available_dates)
     end
 
   end
