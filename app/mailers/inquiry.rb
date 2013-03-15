b677f3f02e7815dc17017e8c5e3adaa7f5accfec
Ryan Buckley <arebuckley@gmail.com>
2013-03-14 21:53:46 -1000

    fix bris email

--- app/mailers/inquiry.rb
+++ app/mailers/inquiry.rb
@@ -1,9 +1,9 @@
 class Inquiry < ActionMailer::Base
-  default :from => 'Tradewinds Cottage <info@tradewindscottage.net>'
+  default from: 'Tradewinds Cottage <info@tradewindscottage.net>'
 
   def new_inquiry(contact)
     @contact = contact
-    mail(to: "Brianna <brianna@tradewindscottage.net, Ryan <arebuckley@gmail.com>", subject: "New inquiry for the Tradewinds Cottage!")
+    mail(to: 'Brianna <brianna@tradewindscottage.net>, Ryan <arebuckley@gmail.com>', subject: 'New inquiry for the Tradewinds Cottage!')
   end
 
 end
